aaa97ef29774f5cfbc86a8cebea99969de0c3a3c
LOKESWARAN ARULJOTHI <11lokeswaran@gmail.com>
2021-08-25 15:24:51 +0530

    serving products from backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/api/products", (req, res) => {
+    res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+    product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
+
+app.listen(port, () => console.log(`Server running on ${port}`));
